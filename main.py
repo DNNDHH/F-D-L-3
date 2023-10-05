10034e09a3f956627a50e032ef715277c261a918
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2023-10-05 15:41:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -70,16 +70,24 @@ def main():
                 time.sleep(2)
                 instance.topHome()
                 time.sleep(2)
-                logger.info('开始友情点召唤!!')
-                instance.drawFP()
-                time.sleep(2)
                 check_blue_apple_cron(instance)
                 logger.info('尝试购买蓝苹果!')
+                
                 try:
                    instance.buyBlueApple(1)
                    time.sleep(2)
                    for _ in range(3):
                       instance.buyBlueApple(1)
+                      time.sleep(2)
+                except Exception as ex:
+                    logger.error(ex)
+                    
+                try:
+                   logger.info('开始友情点召唤!')
+                   instance.drawFP() 
+                   time.sleep(2)
+                   for _ in range(1):
+                      instance.drawFP()
                       time.sleep(2)
                 except Exception as ex:
                     logger.error(ex)
