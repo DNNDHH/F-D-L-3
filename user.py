b35a546a564634fe2e875e442d02f594ec9ee04f
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2024-04-23 00:34:38 +0900

    Update user.py

--- user.py
+++ user.py
@@ -150,6 +150,7 @@ class user:
         self.name_ = hashlib.md5(
             data['cache']['replaced']['userGame'][0]['name'].encode('utf-8')).hexdigest()
         stone = data['cache']['replaced']['userGame'][0]['stone']
+        lv = data['cache']['replaced']['userGame'][0]['lv']
         fpids = data['cache']['replaced']['userGame'][0]['friendCode']
         ticket = 0
         goldenfruit = 0
