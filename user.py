d878c3619d8e7577cfa3a6edf85a70f513d80bba
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2024-05-26 14:19:52 +0900

    Update user.py

--- user.py
+++ user.py
@@ -318,34 +318,68 @@ class user:
         webhook.topLogin(DataWebhook)
 
 
-    def buyBlueApple(self, quantity=1):
-        # https://game.fate-go.jp/shop/purchase
+    def buyBlueApple(self):
+        with open('login.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
 
-        if main.fate_region != "JP":
-            main.logger.error("Region not supported.")
-            return
-        
-        self.builder_.AddParameter('id', '13000000') # 购买蓝苹果 / JP 限定 
-        self.builder_.AddParameter('num', str(quantity))
+            actRecoverAt = data['cache']['replaced']['userGame'][0]['actRecoverAt']
+            actMax = data['cache']['replaced']['userGame'][0]['actMax']
+            carryOverActPoint = data['cache']['replaced']['userGame'][0]['carryOverActPoint']
+            serverTime = data['cache']['serverTime']
+            
+            bluebronzesapling = 0 
+            for item in data['cache']['replaced']['userItem']:
+                if item['itemId'] == 103:
+                    bluebronzesapling = item['num']
+                    break
+                    
+            ap_points = actRecoverAt - serverTime
+            remaining_ap = 0
+            
+            if ap_points > 0:
+               lost_ap_point = (ap_points + 299) // 300
+               if actMax >= lost_ap_point:
+                   remaining_ap = actMax - lost_ap_point
+                   remaining_ap_int = int(remaining_ap)
+            else:
+                remaining_ap = act_max + carryOverActPoint
+                remaining_ap_int = int(remaining_ap)
 
-        data = self.Post(f'{fgourl.server_addr_}/shop/purchase?_userId={self.user_id_}')
-        responses = data['response']
 
-        for response in responses:
-            resCode = response['resCode']
-            resSuccess = response['success']
-            nid = response["nid"]
+               if bluebronzesapling > 0:
+                   quantity = remaining_ap_int // 40
+                   if quantity == 0:
+                       main.logger.info(f"\n ======================================== \n AP低于40，不能购买蓝苹果 \n ======================================== ")
+                       return
+                       
+                   if bluebronzesapling < quantity:
+                       num_to_purchase = bluebronzesapling
+                   else:
+                       num_to_purchase = quantity
 
-            if (resCode != "00"):
-                continue
+                   self.builder_.AddParameter('id', '13000000')
+                   self.builder_.AddParameter('num', str(num_to_purchase))
 
-            if nid == "purchase":
-                if "purchaseName" in resSuccess and "purchaseNum" in resSuccess:
-                    purchaseName = resSuccess['purchaseName']
-                    purchaseNum = resSuccess['purchaseNum']
+                   data = self.Post(f'{fgourl.server_addr_}/shop/purchase?_userId={self.user_id_}')
+                   responses = data['response']
 
-                    main.logger.info(f"{purchaseNum}x {purchaseName} purchased!")
-                    webhook.shop(purchaseName, purchaseNum)
+                   for response in responses:
+                       resCode = response['resCode']
+                       resSuccess = response['success']
+                       nid = response["nid"]
+
+                       if (resCode != "00"):
+                           continue
+
+                       if nid == "purchase":
+                           if "purchaseName" in resSuccess and "purchaseNum" in resSuccess:
+                               purchaseName = resSuccess['purchaseName']
+                               purchaseNum = resSuccess['purchaseNum']
+
+                               main.logger.info(f"\n========================================\n[+] {purchaseNum}x {purchaseName} 购买成功\n========================================")
+                               webhook.shop(purchaseName, purchaseNum)
+               else:
+                   main.logger.info(f"\n ======================================== \n ＞︿＜ 青銅の苗木が足りないヽ (*。>Д<)o゜ \n ======================================== " )
     
 
     def drawFP(self):
