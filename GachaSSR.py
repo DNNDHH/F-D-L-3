30aefd42c1f1bfc2f68fe444a1c0ea5fa481aa02
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2024-10-19 14:10:06 +0800

    Create GachaSSR.py

---
+++ GachaSSR.py
@@ -0,0 +1,75 @@
+import os
+import requests
+import time
+import json
+import fgourl
+import user
+import coloredlogs
+import logging
+
+# Enviroments Variables
+userIds = os.environ['userIds'].split(',')
+authKeys = os.environ['authKeys'].split(',')
+secretKeys = os.environ['secretKeys'].split(',')
+fate_region = os.environ['fateRegion']
+webhook_discord_url = os.environ['webhookDiscord']
+blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
+idempotency_key_signature = os.environ.get('IDEMPOTENCY_KEY_SIGNATURE_SECRET')
+
+
+UA = os.environ['UserAgent']
+
+if UA:
+    fgourl.user_agent_ = UA
+
+userNums = len(userIds)
+authKeyNums = len(authKeys)
+secretKeyNums = len(secretKeys)
+
+logger = logging.getLogger("FGO Daily Login")
+coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+
+def get_latest_verCode():
+    endpoint = ""
+
+    if fate_region == "NA":
+        endpoint += "https://raw.githubusercontent.com/O-Isaac/FGO-VerCode-extractor/NA/VerCode.json"
+    else:
+        endpoint += "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
+
+    response = requests.get(endpoint).text
+    response_data = json.loads(response)
+
+    return response_data['verCode']
+    
+def get_latest_appver():
+    endpoint = "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
+    response = requests.get(endpoint).text
+    response_data = json.loads(response)
+
+    return response_data['appVer']
+
+def main():
+    if userNums == authKeyNums and userNums == secretKeyNums:
+        logger.info('Getting Lastest Assets Info')
+        fgourl.set_latest_assets()
+
+        for i in range(userNums):
+            try:
+                instance = user.user(userIds[i], authKeys[i], secretKeys[i])
+                time.sleep(3)
+                logger.info('登录账号!')
+                instance.topLogin()
+                time.sleep(2)
+                instance.topHome())
+                for _ in range(100): 
+                      instance.drawFF()  #呼符
+                      #instance.drawS()  #石头
+                      time.sleep(0.5)
+                    
+            except Exception as ex:
+                logger.error(ex)
+
+
+if __name__ == "__main__":
+    main()
