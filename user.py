6e8f0311ee736d296894f852b21ed07d278d6386
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2024-11-14 11:17:11 +0800

    Update user.py

--- user.py
+++ user.py
@@ -402,7 +402,7 @@ class user:
         self.builder_.AddParameter('num', '10')
         self.builder_.AddParameter('ticketItemId', '0')
         self.builder_.AddParameter('shopIdIndex', '1')
-        self.builder_.AddParameter('gachaSubId', int(gachaSubId))
+        self.builder_.AddParameter('gachaSubId', str(gachaSubId))
 
         main.logger.info(f"\n ======================================== \n [+] 友情卡池ID : {gachaSubId}\n ======================================== " )
         data = self.Post(f'{fgourl.server_addr_}/gacha/draw?_userId={self.user_id_}')
