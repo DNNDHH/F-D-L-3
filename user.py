08d025d443ddef82e2341649378f9092a190a100
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2025-06-15 20:26:15 +0800

    Update user.py

--- user.py
+++ user.py
@@ -396,8 +396,8 @@ class user:
             
         if gachaSubId == 1000 or gachaSubId is None:
            gachaSubId_mo = 0
-        #else:
-           #gachaSubId_mo = int(gachaSubId) - 1
+        else:
+           gachaSubId_mo = int(gachaSubId) - 1
             
         self.builder_.AddParameter('storyAdjustIds', '[]')
         self.builder_.AddParameter('selectBonusList', '')
