7618ec1184a77e979d85d26d0a2f1e4a5c6f0cc3
Nasu Ni Furenaide <109841184+DNNDHH@users.noreply.github.com>
2025-06-30 18:39:04 +0800

    Update T1S.py

--- T1S.py
+++ T1S.py
@@ -60,6 +60,8 @@ def main():
                 time.sleep(1)
                 instance.lq003()
                 time.sleep(1)
+                instance.buyitem_myseif()
+                time.sleep(1)
                 instance.zc15()
                 
                 #instance.zc16()
